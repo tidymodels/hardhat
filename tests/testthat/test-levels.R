14719cd5539c3b47ca03b48ef5c8362077201090
DavisVaughan <davis@rstudio.com>
2019-02-13 10:50:21 -0500

    Tests for get_levels() and get_outcome_levels()

---
+++ tests/testthat/test-levels.R
@@ -0,0 +1,102 @@
+context("test-levels")
+
+test_that("can extract levels", {
+
+  x <- data.frame(
+    x = factor(letters[1:5]),
+    y = factor(letters[6:10])
+  )
+
+  expect_equal(
+    get_levels(x),
+    list(
+      x = letters[1:5],
+      y = letters[6:10]
+    )
+  )
+
+})
+
+test_that("non-factors are ignored", {
+
+  x <- data.frame(
+    x = factor(letters[1:5]),
+    y = 6:10
+  )
+
+  expect_equal(
+    get_levels(x),
+    list(
+      x = letters[1:5]
+    )
+  )
+
+})
+
+test_that("NULL returned when no factors", {
+
+  x <- data.frame(
+    x = 1:5,
+    y = 6:10
+  )
+
+  expect_equal(
+    get_levels(x),
+    NULL
+  )
+
+})
+
+test_that("Only data frames are allowed", {
+
+  x <- matrix(
+    1:5
+  )
+
+  expect_error(
+    get_levels(x),
+    "A data frame is required"
+  )
+
+})
+
+test_that("Multivariate columns are skipped over", {
+
+  x <- data.frame(x = factor(letters[1:5]))
+  x$y <- matrix(1:10, ncol = 2, dimnames = list(NULL, c("c1", "c2")))
+
+  expect_equal(
+    get_levels(x),
+    list(x = letters[1:5])
+  )
+
+})
+
+test_that("Can extract levels from an outcome", {
+
+  expect_equal(
+    get_outcome_levels(1:5),
+    NULL
+  )
+
+  expect_error(
+    get_outcome_levels("a"),
+    "`y` is of unknown type"
+  )
+
+  expect_equal(
+    get_outcome_levels(factor("a")),
+    list(.outcome = "a")
+  )
+
+  expect_equal(
+    get_outcome_levels(matrix(1:5, dimnames = list(NULL, "c1"))),
+    NULL
+  )
+
+  expect_equal(
+    get_outcome_levels(data.frame(x = factor(letters[1:5]))),
+    list(x = letters[1:5])
+  )
+
+})
