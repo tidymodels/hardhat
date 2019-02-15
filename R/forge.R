27ffba183fb92f601b255487710b6121ffb9eb28
DavisVaughan <davis@rstudio.com>
2019-02-15 18:03:03 -0500

    Finish forge() documentation for formula and recipes methods. Closes #30.

--- R/forge.R
+++ R/forge.R
@@ -174,15 +174,56 @@ forge.default_preprocessor <- function(preprocessor, new_data,
 
 #' Forge - Recipes Method
 #'
-#' If a recipe was used, this is a data.frame that
-#'  is the result of calling [recipes::bake()] with [recipes::all_outcomes()]
-#'  specified.
+#' @description
+#'
+#' For the recipes preprocessor, `forge()` does the following:
+#'
+#' - Calls [shrink()] to trim `new_data` to only the required columns and
+#' coerce `new_data` to a tibble.
+#'
+#' - Calls [scream()] to perform validation on the structure of the columns
+#' of `new_data`.
+#'
+#' - Calls [recipes::bake()] on the `new_data` using the prepped recipe
+#' used during training.
+#'
+#' - Potentially adds an intercept column onto `new_data`,
+#' if the corresponding call to [mold()] used one.
 #'
 #' @inheritParams forge
 #'
 #' @param preprocessor A `"recipes_preprocessor"`.
 #'
 #' @inherit forge return
+#'
+#' @examples
+#'
+#' library(recipes)
+#'
+#' # ---------------------------------------------------------------------------
+#' # Setup
+#'
+#' train <- iris[1:100,]
+#' test <- iris[101:150,]
+#'
+#' # ---------------------------------------------------------------------------
+#' # Recipes Example
+#'
+#' # Create a recipe for the preprocessing
+#' rec <- recipe(Sepal.Width ~ Sepal.Length + Species, iris) %>%
+#'    step_log(Sepal.Length) %>%
+#'    step_dummy(Species)
+#'
+#' # Call mold() with the training data
+#' processed <- mold(rec, train)
+#'
+#' # Then, call forge() with the preprocessor and the test data
+#' # to have it preprocess the test data in the same way
+#' forge(processed$preprocessor, test)
+#'
+#' # Use `outcomes = TRUE` to also extract the preprocessed outcome!
+#' # This logged the Sepal.Length column of `new_data`
+#' forge(processed$preprocessor, test, outcomes = TRUE)
 #'
 #' @rdname forge-recipe
 #' @export
@@ -208,9 +249,38 @@ forge.recipes_preprocessor <- function(preprocessor, new_data,
 
 #' Forge - Formula Method
 #'
-#' If a formula engine was used, this is a data frame
-#'  that is the result of extracting the outcome columns from
-#'  [stats::model.frame()]
+#' @description
+#'
+#' For the terms preprocessor, `forge()` does the following:
+#'
+#' - Calls [shrink()] to trim `new_data` to only the required columns and
+#' coerce `new_data` to a tibble.
+#'
+#' - Calls [scream()] to perform validation on the structure of the columns
+#' of `new_data`.
+#'
+#' - Predictors
+#'
+#'    - Runs [stats::model.frame()] on `new_data` using the stored terms
+#'    object corresponding to the _predictors_.
+#'
+#'    - If, in the original [mold()] call, `indicators = TRUE` was set, it
+#'    then runs [stats::model.matrix()] on the result.
+#'
+#'    - If any offsets are present from using `offset()` in the original call
+#'    to [mold()], then they are extracted with [model_offset()].
+#'
+#'    - If `intercept = TRUE` in the original call to [mold()], then an
+#'    intecept column is added.
+#'
+#'    - Coerces the result of the above steps to a tibble.
+#'
+#'  - Outcomes
+#'
+#'    - Runs [stats::model.frame()] on `new_data` using the stored terms object
+#'    corresponding to the _outcomes_.
+#'
+#'    - Coerces the result to a tibble.
 #'
 #' @inheritParams forge
 #'
@@ -218,8 +288,61 @@ forge.recipes_preprocessor <- function(preprocessor, new_data,
 #'
 #' @inherit forge return
 #'
-#' @rdname forge-formula
+#' @examples
+#' # ---------------------------------------------------------------------------
+#' # Setup
 #'
+#' train <- iris[1:100,]
+#' test <- iris[101:150,]
+#'
+#' # ---------------------------------------------------------------------------
+#' # Formula Example
+#'
+#' # Call mold() with the training data
+#' processed <- mold(
+#'    log(Sepal.Length) ~ Sepal.Length + Species,
+#'    train,
+#'    intercept = TRUE
+#' )
+#'
+#' # Then, call forge() with the preprocessor and the test data
+#' # to have it preprocess the test data in the same way
+#' forge(processed$preprocessor, test)
+#'
+#' # Use `outcomes = TRUE` to also extract the preprocessed outcome
+#' forge(processed$preprocessor, test, outcomes = TRUE)
+#'
+#' # ---------------------------------------------------------------------------
+#' # Dummy variables
+#'
+#' # If factors are not expanded in mold()...
+#' processed <- mold(Sepal.Width ~ Species, train, indicators = FALSE)
+#'
+#' # ...then they aren't expanded in forge() either
+#' forge(processed$preprocessor, test)
+#'
+#' # ---------------------------------------------------------------------------
+#' # Multivariate outcomes
+#'
+#' # Multivariate formulas specified in mold()
+#' # carry over into forge()
+#' processed <- mold(Sepal.Width + log(Sepal.Length) ~ Species, train)
+#'
+#' forge(processed$preprocessor, test, outcomes = TRUE)
+#'
+#' # ---------------------------------------------------------------------------
+#' # Offsets
+#'
+#' # Offsets specified in mold() are computed in forge() as well,
+#' # and are placed in the `$offset` slot of the result
+#' processed <- mold(
+#'    Sepal.Width ~ Species + offset(Sepal.Length) + offset(Petal.Width),
+#'    train
+#' )
+#'
+#' forge(processed$preprocessor, test)
+#'
+#' @rdname forge-formula
 #' @export
 forge.terms_preprocessor <- function(preprocessor, new_data,
                                      outcomes = FALSE, ...) {
