07777d0c25c9afa51b6431b92fe47a0323dcf69a
DavisVaughan <davis@rstudio.com>
2019-02-15 08:28:43 -0500

    Simplify shrink() to only subset columns. Move validation checks into scream(). Closes #22.

---
+++ R/scream.R
@@ -0,0 +1,108 @@
+#' Perform structural checks on `new_data`
+#'
+#' @description
+#'
+#' `scream()` performs a number of validation checks on `new_data`, and yells
+#' loudly if anything is wrong. `scream()` performs the following validation:
+#'
+#' - Checks that the class of each required predictor in `new_data` is the same
+#' as the class used during training.
+#'
+#' - Checks that all `new_data` factor columns don't have any _new_ levels
+#' when compared with the original data used in training. If there are new
+#' levels, they are replaced with `NA` values and a warning is
+#' issued.
+#'
+#' - Checks that all `new_data` factor columns aren't missing any factor levels
+#' when compared with the original data used in training. If there are missing
+#' levels (or misordered levels for ordered factors), then they are restored
+#' and a warning is issued.
+#'
+#' @details
+#'
+#' `scream()` is called by [forge()] after [shrink()] but before the
+#' actual processing is done. Generally, you don't need to call `scream()`
+#' directly, as `forge()` will do it for you.
+#'
+#' If `outcome = TRUE`, then the validation steps are performed on the known
+#' outcome column as well. If [mold()] was called with the XY interface,
+#' then no preprocessing was done to `y` and `outcome` will have no effect
+#' (if a vector was passed as `y` during the fit, `scream()` has no
+#' way of knowing what column in `new_data` corresponds to the outcome).
+#'
+#' If `scream()` is used as a standalone function, it is good practice to call
+#' [shrink()] right before it as there are no checks in `scream()` that ensure
+#' that all of the required column names actually exist in `new_data`. Those
+#' checks exist in `shrink()`.
+#'
+#' @param preprocessor A `"preprocessor"`.
+#'
+#' @param new_data A data frame containing the new data to check the structure
+#' of. This should contain the predictors, and potentially the outcomes if
+#' `outcome = TRUE`.
+#'
+#' @param outcome A logical. Should the outcome be checked as well?
+#'
+#' @return
+#'
+#' A tibble containing the required predictors (and potentially the
+#' outcomes) after any required structural modifications have been made.
+#'
+#' @examples
+#' train <- iris[1:100,]
+#' test <- iris[101:150,]
+#'
+#' # mold() is run at model fit time
+#' # and a terms preprocessor is recorded
+#' x <- mold(log(Sepal.Width) ~ Species, train)
+#'
+#' # Pass that preprocessor to shrink(), along with new_data
+#' # to get a tibble of required predictors back
+#' test_shrunk <- shrink(x$preprocessor, test)
+#'
+#' # Now pass that to scream() to perform validation checks
+#' # Silence is key!
+#' scream(x$preprocessor, test_shrunk)
+#'
+#' # If `outcome = TRUE` is used with shrink(),
+#' # it should also be used with scream()
+#' test_outcome <- shrink(x$preprocessor, test, outcome = TRUE)
+#' scream(x$preprocessor, test_outcome, outcome = TRUE)
+#'
+#' # scream() validates that the classes of `new_data`
+#' # are the same as the ones used in mold(). The below call
+#' # to scream() will fail with an informative error.
+#' test2 <- test
+#' test2$Species <- as.character(test2$Species)
+#'
+#' \dontrun{
+#' scream(x$preprocessor, test2)
+#' }
+#'
+#' @export
+scream <- function(preprocessor, new_data, outcome = FALSE) {
+
+  new_data <- tibble::as_tibble(new_data)
+
+  original_predictor_classes <- preprocessor$predictor_classes
+  original_predictor_levels <- preprocessor$predictor_levels
+
+  validate_new_data_classes(new_data, original_predictor_classes)
+
+  new_data <- check_new_data_novel_levels(new_data, original_predictor_levels)
+  new_data <- check_new_data_level_recovery(new_data, original_predictor_levels)
+
+  if (outcome) {
+
+    original_outcome_classes <- preprocessor$outcome_classes
+    original_outcome_levels <- preprocessor$outcome_levels
+
+    validate_new_data_classes(new_data, original_outcome_classes)
+
+    new_data <- check_new_data_novel_levels(new_data, original_outcome_levels)
+    new_data <- check_new_data_level_recovery(new_data, original_outcome_levels)
+
+  }
+
+  new_data
+}
--- R/shrink.R
+++ R/shrink.R
@@ -2,43 +2,30 @@
 #'
 #' @description
 #'
-#' `shrink()` performs a number of validation steps on `new_data`, and then
-#' subsets it to only contain the required columns. `shrink()` does the
-#' following validation:
+#' `shrink()` subsets `new_data` to only contain the required columns.
+#' `shrink()` also performs the following validation checks on `new_data`:
 #'
 #' - Checks that all required predictor columns exist in `new_data`.
 #'
-#' - Checks that the class of each required predictor in `new_data` is the same
-#' as the class used during training.
-#'
-#' - Checks that all `new_data` factor columns don't have any _new_ levels
-#' when compared with the original data used in training. If there are new
-#' levels, they are replaced with `NA` values and a warning is
-#' issued.
-#'
-#' - Checks that all `new_data` factor columns aren't missing any factor levels
-#' when compared with the original data used in training. If there are missing
-#' levels (or misordered levels for ordered factors), then they are restored
-#' and a warning is issued.
-#'
 #' @details
 #'
-#' If `outcome = TRUE`, then the validation steps are performed on the known
+#' If `outcome = TRUE`, then the validation checks are performed on the known
 #' outcome column as well, and it is returned along with the predictors. If
 #' [mold()] was called with the XY interface, then no preprocessing was done
-#' to `y` and `outcome` cannot be set to `TRUE` (if a vector was passed as `y`
+#' to `y` and `outcome` will have no effect (if a vector was passed as `y`
 #' during the fit, `shrink()` has no way of knowing what column in `new_data`
 #' corresponds to the outcome).
 #'
-#' `shrink()` is called by [forge()] before the actual processing is done.
+#' `shrink()` is called by [forge()] before [scream()] and before the actual
+#' processing is done.
 #'
 #' @param preprocessor A `"preprocessor"`.
 #'
-#' @param new_data A data frame containing the new data that will be shrunk down
-#' to only contain the predictors required for preprocessing and prediction.
+#' @param new_data A data frame at least containing the new predictors, and
+#' potentially the outcomes if `outcome = TRUE`.
 #'
 #' @param outcome A logical. Should the outcome be included in the shrunk
-#' `new_data` as well?
+#' `new_data` output as well?
 #'
 #' @return
 #'
@@ -46,7 +33,6 @@
 #' outcomes).
 #'
 #' @examples
-#'
 #' train <- iris[1:100,]
 #' test <- iris[101:150,]
 #'
@@ -62,19 +48,12 @@
 #' # this can be useful if doing cross validation
 #' shrink(x$preprocessor, test, outcome = TRUE)
 #'
-#' # shrink() validates that the classes are the same
-#' # as the ones used in mold(). The below call
-#' # to shrink() will fail with an informative error.
-#' test2 <- test
-#' test2$Species <- as.character(test2$Species)
-#'
-#' \dontrun{
-#' shrink(x$preprocessor, test2)
-#' }
-#'
+#' # forge() actually preprocesses the new_data, after
+#' # a call to shrink(). Notice how now the Sepal.Width
+#' # column is logged
+#' forge(x$preprocessor, test, outcome = TRUE)$outcomes
 #'
 #' @export
-#'
 shrink <- function(preprocessor, new_data, outcome = FALSE) {
 
   new_data <- tibble::as_tibble(new_data)
@@ -82,24 +61,17 @@ shrink <- function(preprocessor, new_data, outcome = FALSE) {
   cols <- preprocessor$predictors
   validate_predictors(new_data, cols)
 
-  validate_new_data_classes(new_data, preprocessor$predictor_classes)
-
-  original_predictor_levels <- preprocessor$predictor_levels
-  new_data <- check_new_data_novel_levels(new_data, original_predictor_levels)
-  new_data <- check_new_data_level_recovery(new_data, original_predictor_levels)
-
   if (outcome) {
 
     outcome_cols <- preprocessor$outcomes
     validate_outcomes(new_data, outcome_cols)
 
-    validate_new_data_classes(new_data, preprocessor$outcome_classes)
-
-    original_outcome_levels <- preprocessor$outcome_levels
-    new_data <- check_new_data_novel_levels(new_data, original_outcome_levels)
-    new_data <- check_new_data_level_recovery(new_data, original_outcome_levels)
-
     cols <- c(outcome_cols, cols)
+
+    # For the rare chance that an outcome
+    # is also a predictor
+    cols <- unique(cols)
+
   }
 
   # Subset new_data
