a8458704d9032ec5ffbce2bf87ed8675acbe4ca4
DavisVaughan <davis@rstudio.com>
2019-03-06 14:05:27 -0500

    More vctrs R 3.1 bugs

--- R/aaa.R
+++ R/aaa.R
@@ -13,8 +13,21 @@ NULL
 #' @importFrom stats terms
 NULL
 
-# https://github.com/r-lib/vctrs/issues/114
+# R 3.1 bug https://github.com/r-lib/vctrs/issues/114
+#' @importFrom vctrs vec_cast.character
 #' @importFrom vctrs vec_cast.data.frame
+#' @importFrom vctrs vec_cast.Date
+#' @importFrom vctrs vec_cast.difftime
+#' @importFrom vctrs vec_cast.double
+#' @importFrom vctrs vec_cast.factor
+#' @importFrom vctrs vec_cast.integer
+#' @importFrom vctrs vec_cast.integer64
+#' @importFrom vctrs vec_cast.list
+#' @importFrom vctrs vec_cast.logical
+#' @importFrom vctrs vec_cast.POSIXct
+#' @importFrom vctrs vec_cast.POSIXlt
+#' @importFrom vctrs vec_cast.raw
+#' @importFrom vctrs vec_cast.vctrs_list_of
 NULL
 
 # for zeallot
