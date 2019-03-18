1d3215dbcc0518cc7648076ea5d62878c6fe3aa6
DavisVaughan <davis@rstudio.com>
2019-03-18 17:34:28 -0400

    `new_base_model()` -> `new_model()` plus some tweaks to the model constructor

--- R/constructor.R
+++ R/constructor.R
@@ -2,7 +2,7 @@
 #'
 #' A __model__ is a _scalar object_, as classified in
 #' [Advanced R](https://adv-r.hadley.nz/s3.html#object-styles). As such, it
-#' takes uniquely named fields in `...` and combines them into a list with
+#' takes uniquely named elements in `...` and combines them into a list with
 #' a class of `class`. This entire object represent a single model.
 #'
 #' Because every model should have multiple interfaces, including formula
@@ -11,42 +11,60 @@
 #' an engine with all of the information required at prediction time is to
 #' use the one that is returned from a call to [mold()].
 #'
-#' @param engine A preprocessing `engine` returned from a call to [mold()].
-#'
 #' @param ... Name-value pairs for elements specific to the model defined by
 #' `class`.
+#'
+#' @param engine A preprocessing `engine` returned from a call to [mold()].
 #'
 #' @param class A character vector representing the class of the model.
 #'
 #' @examples
-#'
-#' new_base_model(
+#' new_model(
+#'   custom_element= "my-elem",
 #'   engine = default_xy_engine(),
-#'   custom_field = "my-field",
 #'   class = "custom_model"
 #' )
 #'
 #' @export
-new_base_model <- function(engine, ..., class) {
+new_model <- function(..., engine = default_xy_engine(), class = character()) {
 
-  if (rlang::is_missing(engine)) {
-    abort("`engine` is missing, but a preprocessing engine is required.")
-  }
+  validate_is_engine(engine)
+
+  new_abstract_model(..., engine = engine, class = class)
+}
+
+new_abstract_model <- function(..., class = character()) {
 
   if (is_missing(class)) {
     abort("A model `class` must be provided.")
   }
 
-  validate_is_engine(engine)
+  elems <- rlang::list2(...)
+  validate_has_unique_names(elems, "...")
 
-  new_fields <- rlang::list2(...)
-  validate_has_unique_names(new_fields, "...")
+  new_scalar(elems, class = c(class, "hardhat_model"))
+}
 
-  fields <- list(engine = engine)
+new_scalar <- function(elems, ..., class = character()) {
+  check_elems(elems)
+  structure(elems, ..., class = c(class, "hardhat_scalar"))
+}
 
-  fields <- c(fields, new_fields)
+check_elems <- function(elems) {
 
-  structure(fields, class = class)
+  if (!is.list(elems) || length(elems) == 0) {
+    abort("`elems` must be a list of length 1 or greater.")
+  }
+
+  if (!has_unique_names(elems)) {
+    abort("`elems` must have unique names.")
+  }
+
+  if (!identical(names(attributes(elems)), "names")) {
+    abort("`elems` must have no attributes (apart from names).")
+  }
+
+  invisible(elems)
 }
 
 validate_is_engine <- function(engine) {
--- tests/testthat/test-constructor.R
+++ tests/testthat/test-constructor.R
@@ -1,18 +1,27 @@
 context("test-constructor")
 
+test_that("can create new empty models", {
+
+  x <- new_model()
+
+  expect_is(x$engine, "default_xy_engine")
+  expect_is(x, "hardhat_model")
+  expect_is(x, "hardhat_scalar")
+})
+
 test_that("can create new models", {
 
-  x <- new_base_model(engine = default_xy_engine(), class = "custom")
+  x <- new_model(class = "custom")
 
   expect_is(x, "custom")
   expect_is(x$engine, "default_xy_engine")
 })
 
-test_that("can have custom fields", {
+test_that("can have custom elements", {
 
-  x <- new_base_model(
-    default_xy_engine(),
+  x <- new_model(
     y = 1,
+    engine = default_xy_engine(),
     class = "custom_class"
   )
 
@@ -22,13 +31,14 @@ test_that("can have custom fields", {
 test_that("must use a valid engine", {
 
   expect_error(
-    new_base_model(engine = default_xy_engine(), class = "custom"),
+    new_model(engine = default_xy_engine(), class = "custom"),
     NA
   )
 
   expect_error(
-    new_base_model(engine = 1, class = "custom"),
+    new_model(engine = 1, class = "custom"),
     "not a numeric"
   )
 
 })
+
