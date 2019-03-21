d8e1233954294035e4683f3710220570d85a519e
DavisVaughan <davis@rstudio.com>
2019-03-21 10:08:20 -0400

    Fix tests

--- tests/testthat/test-validation.R
+++ tests/testthat/test-validation.R
@@ -1,15 +1,15 @@
 context("test-validation")
 
-test_that("validate_is_univariate()", {
+test_that("validate_outcomes_is_univariate()", {
 
-  expect_silent(validate_is_univariate(data.frame(x = 1)))
+  expect_silent(validate_outcomes_is_univariate(data.frame(x = 1)))
 
-  expect_silent(validate_is_univariate(matrix()))
+  expect_silent(validate_outcomes_is_univariate(matrix()))
 
-  expect_silent(validate_is_univariate(1))
+  expect_silent(validate_outcomes_is_univariate(1))
 
   expect_error(
-    validate_is_univariate(iris),
+    validate_outcomes_is_univariate(iris),
     "`data` must be univariate, but 5 columns were found."
   )
 
