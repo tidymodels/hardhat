dd501a89b07eda8daf3d302adbe93843afc0f5b0
DavisVaughan <davis@rstudio.com>
2019-03-21 10:53:07 -0400

    Improve test coverage of constructor

--- tests/testthat/test-constructor.R
+++ tests/testthat/test-constructor.R
@@ -1,5 +1,30 @@
 context("test-constructor")
 
+test_that("print - hardhat_model", {
+
+  x <- new_model()
+
+  expect_known_output(
+    print(x),
+    test_path("out/print-hardhat_model-1.txt")
+  )
+
+  x2 <- new_model(class = "custom_class")
+
+  expect_known_output(
+    print(x2),
+    test_path("out/print-hardhat_model-2.txt")
+  )
+
+  x3 <- new_model(x = 4, y = "hi", class = "custom_class")
+
+  expect_known_output(
+    print(x3),
+    test_path("out/print-hardhat_model-3.txt")
+  )
+
+})
+
 test_that("can create new empty models", {
 
   x <- new_model()
@@ -42,3 +67,16 @@ test_that("must use a valid blueprint", {
 
 })
 
+test_that("`new_scalar()` must have elements", {
+  expect_error(new_scalar(list()), "must be a list of length 1 or greater")
+})
+
+test_that("`new_scalar()` must have unique names", {
+  expect_error(new_scalar(list(x = 1, x = 2)), "must have unique names")
+})
+
+test_that("`new_scalar()` must have no extra attributes", {
+  x <- list(x = 1)
+  attr(x, "extra") <- 1
+  expect_error(new_scalar(x), "must have no attributes")
+})
