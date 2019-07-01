bfad0f77fe5bb817ca24b81431b8a4842f264fa8
DavisVaughan <davis@rstudio.com>
2019-07-01 12:35:38 -0400

    Tweak predict bridge template

--- inst/templates/R/predict-bridge.R
+++ inst/templates/R/predict-bridge.R
@@ -1,7 +1,11 @@
 predict_{{model}}_bridge <- function(type, model, predictors) {
+  predictors <- as.matrix(predictors)
+
   predict_function <- get_predict_function(type)
   predictions <- predict_function(model, predictors)
+
   hardhat::validate_prediction_size(predictions, predictors)
+
   predictions
 }
 
