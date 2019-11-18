a21c67e91fe54ca800bea3e4f6e5c92c4c099fce
DavisVaughan <davis@rstudio.com>
2019-11-18 15:26:53 -0500

    Allow predictors/outcomes to be `NULL` in a new blueprint

--- R/blueprint.R
+++ R/blueprint.R
@@ -290,15 +290,21 @@ validate_is_ptype_list_or_null <- function(.x, .x_nm) {
   validate_has_name(.x, .x_nm, "predictors")
   validate_has_name(.x, .x_nm, "outcomes")
 
-  validate_is_0_row_tibble(.x$predictors, glue("{.x_nm}$predictors"))
-  validate_is_0_row_tibble(.x$outcomes, glue("{.x_nm}$outcomes"))
+  validate_is_0_row_tibble_or_null(.x$predictors, glue("{.x_nm}$predictors"))
+  validate_is_0_row_tibble_or_null(.x$outcomes, glue("{.x_nm}$outcomes"))
 
   invisible(.x)
 }
 
+validate_is_0_row_tibble_or_null <- function(.x, .x_nm) {
+  if (is.null(.x)) {
+    return(invisible(.x))
+  }
+
+  validate_is_0_row_tibble(.x, .x_nm)
+}
 
 validate_is_0_row_tibble <- function(.x, .x_nm) {
-
   validate_is(.x, tibble::is_tibble, "tibble", .x_nm)
 
   .n <- nrow(.x)
