0bed9ff404f76471a672d3e4161b1aaa39bbff41
Davis Vaughan <davis@rstudio.com>
2019-11-20 13:34:50 -0500

    Add `allow_novel_levels` to `scream()` (#110)

---
+++ tests/testthat/test-scream.R
@@ -0,0 +1,54 @@
+test_that("novel levels can be ignored", {
+  dat <- data.frame(
+    y = 1:4,
+    f = factor(letters[1:4])
+  )
+
+  new <- data.frame(
+    y = 1:5,
+    f = factor(letters[1:5])
+  )
+
+  ptype <- vctrs::vec_ptype(dat)
+
+  expect_warning(
+    x <- scream(new, ptype, allow_novel_levels = TRUE),
+    NA
+  )
+
+  expect_equal(levels(x$f), letters[1:5])
+})
+
+test_that("novel levels in a new character vector can be ignored", {
+  dat <- data.frame(
+    y = 1:4,
+    f = factor(letters[1:4])
+  )
+
+  new <- data.frame(
+    y = 1:5,
+    f = letters[1:5],
+    stringsAsFactors = FALSE
+  )
+
+  ptype <- vctrs::vec_ptype(dat)
+
+  expect_warning(
+    x <- scream(new, ptype, allow_novel_levels = TRUE),
+    NA
+  )
+
+  expect_equal(levels(x$f), new$f)
+})
+
+test_that("ignoring novel levels still passes through incompatible classes", {
+  dat <- data.frame(f = factor(letters[1:4]))
+  new <- data.frame(f = 1:5)
+
+  ptype <- vctrs::vec_ptype(dat)
+
+  expect_error(
+    scream(new, ptype, allow_novel_levels = TRUE),
+    class = "vctrs_error_incompatible_cast"
+  )
+})
