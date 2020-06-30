d08a14a4b314bdf59fd715ae4c1ee012440acb7d
Max Kuhn <mxkuhn@gmail.com>
2020-06-29 21:35:09 -0400

    small data set for showing features

---
+++ R/hardhat_examples.R
@@ -0,0 +1,15 @@
+#' Examples for hardhat
+#'
+#' @details Data objects for a training and test set with the same variables:
+#' three numeric and two factor columns.
+#'
+#'
+#' @name hardhat_examples
+#' @aliases example_train example_test
+#' @docType data
+#' @return \item{example_train,example_test}{tibbles}
+#'
+#' @keywords datasets
+#' @examples
+#' data(hardhat_examples)
+NULL
