239dd032bccf3f977fd26eebe09924601d430092
DavisVaughan <davis@rstudio.com>
2020-06-30 11:50:10 -0400

    Tweak `indicators` argument documentation

--- R/blueprint-formula.R
+++ R/blueprint-formula.R
@@ -2,10 +2,19 @@
 #' predictors and outcomes should be preprocessed. This argument is set
 #' automatically at [mold()] time.
 #'
-#' @param indicators A character string with possible values "none" (no dummy
-#' variables), "traditional" (create dummy variables using the usual methods),
-#' or "one-hot" (create dummy variables for all levels).
+#' @param indicators A single character string. Control how factors are
+#' expanded into dummy variable indicator columns. One of:
 #'
+#'   - `"traditional"` - The default. Create dummy variables using the
+#'   traditional [model.matrix()] infrastructure. Generally this creates
+#'   `K - 1` indicator columns for each factor, where `K` is the number of
+#'   levels in that factor.
+#'
+#'   - `"none"` - Leave factor variables alone. No expansion is done.
+#'
+#'   - `"one-hot"` - Create dummy variables using a one-hot encoding approach
+#'   that expands unordered factors into all `K` indicator columns, rather than
+#'   `K - 1`.
 #'
 #' @rdname new-blueprint
 #' @export
