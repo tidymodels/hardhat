6f8ffb826c3b75b56aaf6ea524ea69af3f8e7611
DavisVaughan <davis@rstudio.com>
2020-06-30 12:04:44 -0400

    Move `contr_one_hot()` near `model_matrix_one_hot()`
    Also fixes integer handling in `contr_one_hot()` and tweaks documentation

--- R/model-matrix.R
+++ R/model-matrix.R
@@ -97,21 +97,6 @@ model_matrix <- function(terms, data) {
   tibble::as_tibble(predictors)
 }
 
-model_matrix_one_hot <- function(terms, data) {
-  # In case the option is somehow unset
-  default_contrast <- c(unordered = "contr.treatment", ordered = "contr.poly")
-
-  contrasts <- getOption("contrasts", default = default_contrast)
-
-  # Use custom contrast function as the default for unordered factors
-  contrasts["unordered"] <- "contr_one_hot"
-
-  rlang::with_options(
-    model_matrix(terms = terms, data = data),
-    contrasts = contrasts
-  )
-}
-
 strip_model_matrix <- function(x) {
   colnames <- colnames(x)
   dimnames <- list(NULL, colnames)
@@ -136,4 +121,67 @@ validate_is_terms <- function(.x, .x_nm) {
   }
 
   validate_is(.x, is_terms, "terms object", .x_nm)
+}
+
+# ------------------------------------------------------------------------------
+
+model_matrix_one_hot <- function(terms, data) {
+  # In case the option is somehow unset
+  default_contrast <- c(unordered = "contr.treatment", ordered = "contr.poly")
+
+  contrasts <- getOption("contrasts", default = default_contrast)
+
+  # Use custom contrast function as the default for unordered factors
+  contrasts["unordered"] <- "contr_one_hot"
+
+  rlang::with_options(
+    model_matrix(terms = terms, data = data),
+    contrasts = contrasts
+  )
+}
+
+#' Contrast function for one-hot encodings
+#'
+#' This contrast function produces a model matrix that has indicator columns for
+#' each level of each factor.
+#'
+#' @param n A vector of character factor levels or the number of unique levels.
+#' @param contrasts This argument is for backwards compatibility and only the
+#'   default of `TRUE` is supported.
+#' @param sparse This argument is for backwards compatibility and only the
+#'   default of `FALSE` is supported.
+#'
+#' @return A diagonal matrix that is `n`-by-`n`.
+#'
+#' @keywords internal
+contr_one_hot <- function(n, contrasts = TRUE, sparse = FALSE) {
+  if (sparse) {
+    rlang::warn("`sparse = TRUE` not implemented for `contr_one_hot()`.")
+  }
+
+  if (!contrasts) {
+    rlang::warn("`contrasts = FALSE` not implemented for `contr_one_hot()`.")
+  }
+
+  if (is.character(n)) {
+    names <- n
+    n <- length(names)
+  } else if (is.numeric(n)) {
+    n <- as.integer(n)
+
+    if (length(n) != 1L) {
+      rlang::abort("`n` must have length 1 when an integer is provided.")
+    }
+
+    names <- as.character(seq_len(n))
+  } else {
+    rlang::abort("`n` must be a character vector or an integer of size 1.")
+  }
+
+  out <- diag(n)
+
+  rownames(out) <- names
+  colnames(out) <- names
+
+  out
 }
