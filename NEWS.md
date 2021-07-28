6b547ef13458d0d84f4ea3e701b8f403c46b4bcf
DavisVaughan <davis@rstudio.com>
2021-07-28 14:09:41 -0400

    Invert `prep_specific_roles` as `bake_dependent_roles`

--- NEWS.md
+++ NEWS.md
@@ -1,16 +1,19 @@
 # hardhat (development version)
 
-* `default_recipe_blueprint()` has gained a new `prep_specific_roles` argument,
-  which allows you to specify a character vector of recipes "roles"
-  corresponding to columns that are required at `recipes::prep()` time, but
-  are not required at `recipes::bake()` time. For example, you might have a
-  custom `"case_weights"` role that is required for computing case-weighted
-  estimates at prep time, but isn't required when baking against new data
-  (since the estimates have already been pre-computed and stored). Columns
-  with roles matching `prep_specific_roles` will be included in the `$extras`
-  slot of the result of `mold()`, but will not be included in the `$extras`
-  slot of the result of `forge()`, and will be excluded from the structural
-  checks done by `forge()`.
+* Recipe preprocessors now ignore non-standard recipe roles (i.e. not
+  `"outcome"` or `"predictor"`) by default when calling `forge()`. Previously,
+  it was assumed that all non-standard role columns present in the original
+  training data were also required in the test data when `forge()` is called.
+  It seems to be more often the case that those columns are actually not
+  required to `bake()` new data, and often won't even be present when making
+  predictions on new data. For example, a custom `"case_weights"` role might be
+  required for computing case-weighted estimates at `prep()` time, but won't
+  be necessary at `bake()` time (since the estimates have already been
+  pre-computed and stored). To account for the case when you do require a
+  specific non-standard role to be present at `bake()` time,
+  `default_recipe_blueprint()` has gained a new argument,
+  `bake_dependent_roles`, which can be set to a character vector of
+  non-standard roles that are required.
 
 * `use_modeling_files()` and `create_modeling_package()` no longer open the
   package documentation file in the current RStudio session (#192).
--- R/blueprint-recipe.R
+++ R/blueprint-recipe.R
@@ -4,15 +4,16 @@
 #' @param fresh Should already trained operations be re-trained when `prep()` is
 #'   called?
 #'
-#' @param prep_specific_roles A character vector of recipes "roles"
-#'   corresponding to columns that are only required at `prep()` time, and are
-#'   not required at `bake()` time. These columns are excluded from checks done
-#'   in [forge()] to validate the column structure of `new_data`, will not be
-#'   passed to `bake()` even if they existed in `new_data`, and will not be
-#'   returned in the `forge()$extras$roles` slot. Any custom role can be
-#'   specified (such as `"case_weights"`), but `"outcome"` and `"predictor"`
-#'   roles are not allowed. See the documentation of [recipes::add_role()] for
-#'   more information about roles.
+#' @param bake_dependent_roles A character vector of recipes column "roles"
+#'   specifying roles that are required to [recipes::bake()] new data.
+#'   `"outcome"` and `"predictor"` roles are always required to be able to call
+#'   `bake()`, but typically non-standard roles (such as `"id"` or
+#'   `"case_weights"`) are not required. Unless specified
+#'   by `bake_dependent_roles`, non-standard role columns are excluded from
+#'   checks done in [forge()] to validate the column structure of `new_data`,
+#'   will not be passed to `bake()` even if they existed in `new_data`, and will
+#'   not be returned in the `forge()$extras$roles` slot. See the documentation
+#'   of [recipes::add_role()] for more information about roles.
 #'
 #' @rdname new-blueprint
 #' @export
@@ -21,7 +22,7 @@ new_recipe_blueprint <- function(mold,
                                  intercept = FALSE,
                                  allow_novel_levels = FALSE,
                                  fresh = TRUE,
-                                 prep_specific_roles = character(),
+                                 bake_dependent_roles = character(),
                                  composition = "tibble",
                                  ptypes = NULL,
                                  recipe = NULL,
@@ -37,7 +38,7 @@ new_recipe_blueprint <- function(mold,
   )
 
   validate_is_bool(fresh)
-  validate_prep_specific_roles(prep_specific_roles)
+  validate_bake_dependent_roles(bake_dependent_roles)
 
   validate_is_recipe_or_null(recipe)
 
@@ -47,7 +48,7 @@ new_recipe_blueprint <- function(mold,
     intercept = intercept,
     allow_novel_levels = allow_novel_levels,
     fresh = fresh,
-    prep_specific_roles = prep_specific_roles,
+    bake_dependent_roles = bake_dependent_roles,
     composition = composition,
     ptypes = ptypes,
     recipe = recipe,
@@ -79,16 +80,16 @@ validate_is_recipe_or_null <- function(recipe) {
   validate_is_or_null(recipe, is_recipe, "recipe")
 }
 
-validate_prep_specific_roles <- function(prep_specific_roles) {
-  validate_is_character(prep_specific_roles, "prep_specific_roles")
+validate_bake_dependent_roles <- function(bake_dependent_roles) {
+  validate_is_character(bake_dependent_roles, "bake_dependent_roles")
 
-  if (anyNA(prep_specific_roles)) {
-    abort("`prep_specific_roles` can't contain missing values.")
+  if (anyNA(bake_dependent_roles)) {
+    abort("`bake_dependent_roles` can't contain missing values.")
   }
 
-  if (any(prep_specific_roles %in% c("outcome", "predictor"))) {
-    abort("`prep_specific_roles` can't be 'outcome' or 'predictor'.")
+  if (any(bake_dependent_roles %in% c("outcome", "predictor"))) {
+    abort("`bake_dependent_roles` can't be 'outcome' or 'predictor', as these are already required.")
   }
 
-  invisible(prep_specific_roles)
+  invisible(bake_dependent_roles)
 }
