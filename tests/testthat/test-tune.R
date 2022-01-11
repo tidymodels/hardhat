0932f713ffb7a3c8eb5df9a55fa46a09dbd37eda
DavisVaughan <davis@rstudio.com>
2022-01-11 09:00:10 -0500

    Revise tests

--- tests/testthat/test-tune.R
+++ tests/testthat/test-tune.R
@@ -1,21 +1,16 @@
-
-test_that('tune creates a call', {
+test_that("tune creates a call", {
   expect_true(is.call(tune()))
-  expect_true(is.call(tune(id = "salad")))
-  expect_true(is.call(tune("steak")))
-
+  expect_true(is.call(tune("foo")))
 })
 
-test_that('tune id value', {
-  expect_equal(tune(), call("tune"))
-  expect_equal(tune(""), call("tune"))
-  expect_equal(tune(id = "salad"), call("tune", "salad"))
-  expect_equal(tune("steak"), call("tune", "steak"))
+test_that("tune `id` value", {
+  expect_identical(tune(), call("tune"))
+  expect_identical(tune(""), call("tune"))
+  expect_identical(tune("foo"), call("tune", "foo"))
 })
 
-test_that('bad tune id', {
-  expect_error(tune(2))
-  expect_error(tune(NA_character_), "The `id` cannot be NA.")
-  expect_error(tune(NULL))
-  expect_error(tune(a = "a"), 'unused argument')
+test_that("`id` is validated", {
+  expect_error(tune(1), "The `id` should be a single character string.")
+  expect_error(tune(c("x", "y")), "The `id` should be a single character string.")
+  expect_error(tune(NA_character_), "The `id` can't be missing.")
 })
