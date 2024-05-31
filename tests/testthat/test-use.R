7dc1d32c5790feb7505becae087559e660201b4d
Davis Vaughan <davis@posit.co>
2024-05-31 15:10:07 -0400

    Use `local_project()` and withr
    To align more with new usethis. I noticed that the tests fail if you run `devtools::test()` interactively two times in a row and this seems to be related to that.

--- tests/testthat/test-use.R
+++ tests/testthat/test-use.R
@@ -2,21 +2,18 @@ test_that("can create a modeling package", {
   skip_on_cran()
   local_options(usethis.quiet = TRUE)
 
-  dir_base <- tempdir()
-  dir_pkg <- file.path(dir_base, "model")
-  dir.create(dir_pkg)
-  on.exit(unlink(dir_pkg, recursive = TRUE), add = TRUE)
+  dir <- withr::local_tempdir("model")
 
   model <- "linear_regression"
 
   # `usethis.quiet = TRUE` silences most of the messages, but there is an
   # unavoidable `i Loading model` that we get from devtools if we don't do this
   suppressMessages({
-    create_modeling_package(dir_pkg, model, open = FALSE)
+    create_modeling_package(dir, model, open = FALSE)
   })
 
-  top_level_files <- list.files(dir_pkg)
-  script_files <- list.files(file.path(dir_pkg, "R"))
+  top_level_files <- list.files(dir)
+  script_files <- list.files(file.path(dir, "R"))
 
   expect_true("DESCRIPTION" %in% top_level_files)
   expect_true("R" %in% top_level_files)
@@ -32,10 +29,7 @@ test_that("can add a second model to a modeling package", {
   skip_on_cran()
   local_options(usethis.quiet = TRUE)
 
-  dir_base <- tempdir()
-  dir_pkg <- file.path(dir_base, "model")
-  dir.create(dir_pkg)
-  on.exit(unlink(dir_pkg, recursive = TRUE), add = TRUE)
+  dir <- withr::local_tempdir("model")
 
   model1 <- "linear_regression"
   model2 <- "random_forest"
@@ -43,18 +37,12 @@ test_that("can add a second model to a modeling package", {
   # `usethis.quiet = TRUE` silences most of the messages, but there is an
   # unavoidable `i Loading model` that we get from devtools if we don't do this
   suppressMessages({
-    create_modeling_package(dir_pkg, model1, open = FALSE)
+    create_modeling_package(dir, model1, open = FALSE)
   })
 
-  with_dir <- function(new, code) {
-    old <- setwd(dir = new)
-    on.exit(setwd(old))
-    force(code)
-  }
+  usethis::with_project(dir, use_modeling_files(model2))
 
-  with_dir(dir_pkg, use_modeling_files(model2))
-
-  script_files <- list.files(file.path(dir_pkg, "R"))
+  script_files <- list.files(file.path(dir, "R"))
 
   expect_true(glue::glue("{model1}-constructor.R") %in% script_files)
   expect_true(glue::glue("{model1}-fit.R") %in% script_files)
