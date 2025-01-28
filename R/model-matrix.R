ae9f5da5054318d6f352a2d645372e7d8e3676ef
Hannah Frick <hannah@posit.co>
2025-01-28 12:03:38 +0100

    Use parsnip's version of the function

--- R/model-matrix.R
+++ R/model-matrix.R
@@ -171,7 +171,8 @@ model_matrix_one_hot <- function(terms, data, ..., call = caller_env()) {
 #' This contrast function produces a model matrix that has indicator columns for
 #' each level of each factor.
 #'
-#' @param n A vector of character factor levels or the number of unique levels.
+#' @param n A vector of character factor levels (of length >=1) or the number
+#' of unique levels (>= 1).
 #' @param contrasts This argument is for backwards compatibility and only the
 #'   default of `TRUE` is supported.
 #' @param sparse This argument is for backwards compatibility and only the
@@ -182,17 +183,23 @@ model_matrix_one_hot <- function(terms, data, ..., call = caller_env()) {
 #' @keywords internal
 contr_one_hot <- function(n, contrasts = TRUE, sparse = FALSE) {
   if (sparse) {
-    cli::cli_warn("{.code sparse = TRUE} not implemented for {.fn contr_one_hot}.")
+    cli::cli_warn("{.code sparse = TRUE} not implemented for {.fun contr_one_hot}.")
   }
 
   if (!contrasts) {
-    cli::cli_warn("{.code contrasts = FALSE} not implemented for {.fn contr_one_hot}.")
+    cli::cli_warn(
+      "{.code contrasts = FALSE} not implemented for {.fun contr_one_hot}."
+    )
   }
 
   if (is.character(n)) {
+    if (length(n) < 1) {
+      cli::cli_abort("{.arg n} cannot be empty.")
+    }
     names <- n
     n <- length(names)
   } else if (is.numeric(n)) {
+    check_number_whole(n, min = 1)
     n <- as.integer(n)
 
     if (length(n) != 1L) {
@@ -201,7 +208,7 @@ contr_one_hot <- function(n, contrasts = TRUE, sparse = FALSE) {
 
     names <- as.character(seq_len(n))
   } else {
-    cli::cli_abort("{.arg n} must be a character vector or an integer of size 1.")
+    check_number_whole(n, min = 1)
   }
 
   out <- diag(n)
